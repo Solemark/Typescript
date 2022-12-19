9b27a2279e6b554eeae74a80b58357ac2342b811
Solemarc <62088534+Solemark@users.noreply.github.com>
2022-12-19 17:41:18 +1000

    Add files via upload

--- fizz-buzz.ts
+++ fizz-buzz.ts
@@ -1,18 +1,16 @@
 export const fizzBuzz = (max: number, fizz: number, buzz: number) => {
     let output = ''
-    let str = ''
     for (let i = 1; i <= max; i++) {
-        str = ''
         if (i % fizz == 0) {
-            str += 'fizz'
+            output += 'fizz'
         }
         if (i % buzz == 0) {
-            str += 'buzz'
+            output += 'buzz'
         }
-        if (str == '') {
-            str += i
+        if (output[output.length - 1] != 'z') {
+            output += i
         }
-        output += str + '\n'
+        output += '\n'
     }
     return output
 }
