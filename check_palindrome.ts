b8f92db31c5cdc50e61cae2388e28652f0c1658a
Solemark <larco9mason@gmail.com>
2023-07-14 20:48:57 +1000

    formatting of check_palindrome and sum_array

--- check_palindrome.ts
+++ check_palindrome.ts
@@ -1,16 +1,16 @@
 export const checkPalindrome = (str: string): boolean => {
     let x = 0
     let y = str.length - 1
-    if(typeof str === 'string'){
-        while(x < y){
-            if(str[x] != str[y]){
+    if (typeof str === 'string') {
+        while (x < y) {
+            if (str[x] != str[y]) {
                 return false
             }
             x++
             y--
         }
         return true
-    }else{
+    } else {
         return false
     }
 }
